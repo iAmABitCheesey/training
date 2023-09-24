038b0cfc05ea20827f15743f66d57db840ec4f7b
Me <noneof@your.business>
2023-09-24 13:42:43 +0200

    Added login

---
+++ index.js
@@ -0,0 +1,13 @@
+//TODO: Add routes
+
+const express = require("express");
+const path = require('path')
+
+const app = express();
+const port = 3000;
+
+app.use('/', express.static(path.join(__dirname, 'src/login')));
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
